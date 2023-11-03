71b6e2212b7df851254ad28c4f51fc9568259254
Binnukrishna <binnukrishna5233@gmail.com>
2023-11-03 17:53:06 +0530

    mobile footer prototype set

---
+++ scripts/footerbg-script.js
@@ -0,0 +1,14 @@
+const image = document.getElementById("footerbg");
+
+function updateImageSource() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth > 480) {
+    image.src = "../assets/images/footerbgDesktop.jpg";
+  } else {
+    image.src = "../assets/images/footerbg.jpg.jpg";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", updateImageSource);
+window.addEventListener("resize", updateImageSource);
