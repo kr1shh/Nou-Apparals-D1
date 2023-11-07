309292c5fa8ec14e62a1af26e78f70502e6a01dc
Binnukrishna <binnukrishna5233@gmail.com>
2023-11-07 17:02:54 +0530

    about hero done

--- components/components.js
+++ components/components.js
@@ -40,7 +40,7 @@ class Nav_bar extends HTMLElement {
           <div class="nav-links">
               <ul>
                   <li><a href="/">HOME</a></li>
-                  <li><a href="#">ABOUT</a></li>
+                  <li><a href="../pages/about-page.html">ABOUT</a></li>
                   <li><a href="../pages/services-page.html">SERVICES</a></li>
                   <li><a href="../pages/blog-page.html">BLOG</a></li>
                   <li><a href="#">GALLERY</a></li>
@@ -57,7 +57,7 @@ class Nav_bar extends HTMLElement {
           <div class="mob-nav">
               <ul>
                   <li><a href="/">HOME</a></li>
-                  <li><a href="#">ABOUT</a></li>
+                  <li><a href="../pages/about-page.html">ABOUT</a></li>
                   <li><a href="../pages/services-page.html">SERVICES</a></li>
                   <li><a href="../pages/blog-page.html">BLOG</a></li>
                   <li><a href="#">GALLERY</a></li>
